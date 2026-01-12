a47550c620600536ea9bebe7fddd9f6779e5839b
Natnael Deribe <88533508+Natink1@users.noreply.github.com>
2026-01-12 11:49:20 +0300

    column and remnaining days

--- app/Filament/Resources/Devices/DeviceResource.php
+++ app/Filament/Resources/Devices/DeviceResource.php
@@ -20,7 +20,7 @@ class DeviceResource extends Resource
 {
     protected static ?string $model = Device::class;
 
-    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
+    protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedPrinter;
 
     protected static ?string $recordTitleAttribute = 'Device';
 
